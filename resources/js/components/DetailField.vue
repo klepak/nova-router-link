236bb612218539f90236ec298ce707a67abebf14
Krot Eval <38257723+4n70w4@users.noreply.github.com>
2020-01-24 19:26:59 +0300

    #1 pass query to router-link

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,7 +1,7 @@
 <template>
     <panel-item :field="field">
         <template slot="value">
-            <router-link tag="a" :to="{name: field.route, params: field.params}" class="no-underline font-bold dim text-primary">
+            <router-link tag="a" :to="{name: field.route, params: field.params, query: field.query}" class="no-underline font-bold dim text-primary">
                 {{ field.value }}
             </router-link>
         </template>
